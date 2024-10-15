7b7da845ba6e5a2674dfae2937ddc18ac044c7cd
Theo Smith <tls54@outlook.com>
2024-10-15 14:25:54 +0100

    split transfer functions into new file for better readability

---
+++ Extraction/transfer_functions.py
@@ -0,0 +1,42 @@
+import numpy as np
+from Extraction import c
+
+## Define transfer function for no reflection model
+
+def H_th_function(n, w, length):
+    '''
+    Inputs
+    ------
+    n: refractive index
+    w: frequency of light being propagated
+    length: length of the sample we are modelling
+    
+    outputs
+    -------
+    returns: output for the tranfer function
+
+    Method
+    ------
+    Equation for transfer function derived from [Input source here]
+
+    '''
+    return (4 * n) / ((n + 1) ** 2) * np.exp(-1j * (n - 1) * w * length/ c)
+
+def H_prime_function(n, w, length):
+    '''
+    Inputs
+    ------
+    n: refractive index
+    w: frequency of light being propagated
+    length: length of the sample we are modelling
+    
+    outputs
+    -------
+    returns: derivative of transfer function, calculated analytically
+
+    Method
+    ------
+    Equation for derivative of transfer function formulated by hand 
+    
+    '''
+    return (1 / n) - (2 / (n + 1)) - 1j * w * length / c
