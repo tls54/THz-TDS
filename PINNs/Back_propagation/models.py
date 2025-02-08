0fa2242b216e46a61590f0f7616e72aa7673d72a
Theo Smith <tls54@outlook.com>
2025-02-08 14:11:27 +0000

    Minor updates

--- PINNs/Back_propagation/models.py
+++ PINNs/Back_propagation/models.py
@@ -8,18 +8,13 @@ class TransferFunctionModel(torch.nn.Module):
         super().__init__()
         self.w_tensor = w_tensor  # Store frequency range
         # Directly initialize parameters within expected range
-        self.n = torch.nn.Parameter(torch.tensor(3.0, dtype=torch.float32))  # n > 0
-        self.k = torch.nn.Parameter(torch.tensor(-0.05, dtype=torch.float32))  # k < 0
-        #self.d = torch.nn.Parameter(torch.tensor(400e-6, dtype=torch.float32))  # d > 0
+        self.n = torch.nn.Parameter(torch.tensor(3.0, dtype=torch.float32))  
+        self.k = torch.nn.Parameter(torch.tensor(-0.05, dtype=torch.float32))  
+        #self.d = torch.nn.Parameter(torch.tensor(400e-6, dtype=torch.float32))  
         self.d = d
 
     def forward(self):
         # Ensure parameters satisfy constraints
-        """
-        n = torch.abs(self.n)  # Ensure n > 0
-        k = -torch.abs(self.k)  # Ensure k < 0
-        d = torch.abs(self.d)  # Ensure d > 0
-        """
         n = self.n # Ensure n > 0
         k = self.k  # Ensure k < 0
         #d = self.d  # Ensure d > 0
