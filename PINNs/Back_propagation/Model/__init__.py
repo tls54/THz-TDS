af44cee921778bea285f7e0ec66f962e38f25ac6
Theo Smith <tls54@outlook.com>
2025-03-06 16:18:22 +0000

    Model updated to perform phase unwrapping from consistent start point.

---
+++ PINNs/Back_propagation/Model/__init__.py
@@ -0,0 +1,4 @@
+from .models import TransferFunctionModel
+from .back_prop_utils import H_th_function, round_to_sig_figs, c
+from .loss_functions import loss
+from .plotting_utils import plot_transfer, plot_comparison, plot_loss_contributions
